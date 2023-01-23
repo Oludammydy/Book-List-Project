9ec3357e1717ee3e058d823f0d34632527bcb339
oludammydy <oludammydy@gmail.com>
2023-01-23 14:14:44 +0100

    Form now shows Success and Error message

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ UI.prototype.addBookToList = function (book) {
   list.appendChild(row);
 }
 
+// Show Alerts
+UI.prototype.showAlert = function (message, className) {
+  // Create div for the element
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add Text
+  div.appendChild(document.createTextNode(message));
+  // Get Parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
+
+  // Timeout after 3 sec
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 // Clear fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -46,13 +67,21 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   // Istantiate UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+    //Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
 
+    // Show success
+    ui.showAlert('Book Added!', 'success');
 
-  // Clear fields
-  ui.clearFields();
+    // Clear fields
+    ui.clearFields();
 
+  }
   // console.log(title, author, isbn);
 
   e.preventDefault();
